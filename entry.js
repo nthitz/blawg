c7a867cf02b841d27ce134bd8313abb529aa615c
Nick Yahnke <nthitz@gmail.com>
2015-07-13 22:37:48 -0700

    moved stuff around

--- entry.js
+++ entry.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Router from 'react-router'
-import Routes from './Routes.jsx'
+import Routes from './app/Routes.jsx'
 
 if (typeof document !== 'undefined') {
   var initialProps = JSON.parse(document.getElementById('initial-props').innerHTML)
