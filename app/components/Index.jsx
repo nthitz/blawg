3ddf74baa1a795ef0bb359ab7b924c5c8bdb6682
Nick Yahnke <nthitz@gmail.com>
2015-07-15 23:47:23 -0700

    pyramid

--- app/components/Index.jsx
+++ app/components/Index.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
+import PyramidWireframe from './PyramidWireframe.js'
 import HelloWorldScene from './HelloWorldScene.js'
 export default class Index extends React.Component {
   render () {
     return (
       <main>
         <span>Index component</span>
+        <PyramidWireframe />
         <HelloWorldScene dimensions={300} spinSpeedY={1} />
         <HelloWorldScene spinSpeedY={2} spinSpeedX={1.5} />
       </main>
---
+++ app/components/PyramidWireframe.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+var THREE = require('threejs')
+
+import ThreeScene from './ThreeScene.jsx'
+
+export default class PyramidWireframe extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.sceneObjects = []
+
+    this.init = this.init.bind(this)
+    this.animate = this.animate.bind(this)
+  }
+  componentDidMount() {
+    this.refs.scene.init()
+  }
+  init() {
+    var wireframeMaterial = new THREE.MeshNormalMaterial({
+      wireframe: true
+    });
+    var normalMaterial = new THREE.MeshNormalMaterial({
+    });
+
+
+    this.sceneObjects.pyramid = new THREE.Mesh(
+      new THREE.TetrahedronGeometry(100, 0),
+      wireframeMaterial
+    );
+   this.sceneObjects.pyramidInner = new THREE.Mesh(
+      new THREE.TetrahedronGeometry(70, 0),
+      normalMaterial
+    );
+    this.sceneObjects.pyramidInner.rotation.y += Math.PI / 2
+    this.refs.scene.scene.add(this.sceneObjects.pyramid);
+    this.refs.scene.scene.add(this.sceneObjects.pyramidInner);
+
+
+  }
+
+  animate() {
+    _.each(['pyramid','pyramidInner'], (key) => {
+      var object = this.sceneObjects[key];
+      object.rotation.x += 0.01 * this.props.spinSpeedX;
+      object.rotation.y += 0.01 * this.props.spinSpeedY;
+    })
+  }
+  render() {
+    return <div>
+      <ThreeScene ref="scene" init={this.init} animate={this.animate} {...this.props} />
+    </div>
+  }
+}
+PyramidWireframe.propTypes = {
+  spinSpeedX: React.PropTypes.number,
+  spinSpeedY: React.PropTypes.number
+}
+PyramidWireframe.defaultProps = {
+  spinSpeedX: 0,
+  spinSpeedY: 1
+};
